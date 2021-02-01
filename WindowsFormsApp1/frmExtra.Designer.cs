430cf2c97f72c37a8c5beadfe49d108660fee8c3
andreaBarberis <a.barberis.0996@vallauri.edu>
2021-02-01 12:51:12 +0100

    imgExtra

--- WindowsFormsApp1/frmExtra.Designer.cs
+++ WindowsFormsApp1/frmExtra.Designer.cs
@@ -68,7 +68,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(295, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(285, 325);
             this.Controls.Add(this.btnWeb);
             this.Controls.Add(this.btnPlaylist);
             this.Controls.Add(this.btnAB);
